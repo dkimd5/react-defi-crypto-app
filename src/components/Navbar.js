0320ae067c479b1a453deddd2b3cbb3631ba1315
dkimd5 <dkimd5@mail.ru>
2022-02-28 15:58:08 +0300

    Add toggle sidemenu logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <div className="header">
       <div className="container">
         <h1>
           De<span className="primary">Fi</span>
         </h1>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li>
             <a href="/">Home</a>
           </li>
@@ -26,8 +29,12 @@ function Navbar() {
         <div className="btn-group">
           <button className="btn">Connect Wallet</button>
         </div>
-        <div className="hamburger">
-          <FaBars size={20} style={{ color: "#333" }} />
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={20} style={{ color: "#333" }} />
+          ) : (
+            <FaBars size={20} style={{ color: "#333" }} />
+          )}
         </div>
       </div>
     </div>
